5ec6a34a2b56a097697a8a51f292928d2487b605
Inside_the_Soul <100500024q>
2022-06-06 19:41:42 +0300

    Prepared infrastructure for main screens

--- core/core-navigation/build.gradle.kts
+++ core/core-navigation/build.gradle.kts
@@ -8,6 +8,9 @@ dependencies {
 
     implementation(project(":core:core-feature"))
 
+    implementation(project(":features:feature-search"))
+    implementation(project(":features:feature-favourite"))
+
     addNavigationComponentDependencies()
     addDaggerDependencies()
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "XKCDMvpProject"
+include(":app")
+include(":data")
+include(":domain")
+include(":core:core-feature")
+include(":core:core-navigation")
+include(":features:feature-search")
+include(":features:feature-favourite")
+include(":features:feature-comic-details")
